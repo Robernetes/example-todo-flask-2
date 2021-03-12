1240a5253b77944e4372da7e3be7edbae8333fde
raagus <raaguilarm@gmail.com>
2021-03-12 02:11:25 -0500

    Add services: get_users and get_todos

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -8,4 +8,8 @@ firebase_admin.initialize_app(credential)
 db = firestore.client()
 
 def get_users():
-    return db.collection('users').get('users')
+    return db.collection('users').get()
+
+
+def get_todos(user_id):
+    return db.collection('users').document(user_id).collection('todos').get()
