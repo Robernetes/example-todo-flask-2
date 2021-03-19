ca1cabdbbb8172066dfa73b7d6931e106407d24a
raagus <raaguilarm@gmail.com>
2021-03-19 10:24:38 -0500

    Add condition to validate password

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,9 @@
 from flask import render_template, session, flash, redirect, url_for
 from app.forms import LoginForm
 from . import auth
+from app.firestore_service import get_user
+from app.models import UserData, UserModel
+from flask_login import login_user
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
@@ -10,8 +13,27 @@ def login():
     }
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
-        flash('Nombre de usuario registrado exitosamente!!!')
+        password = login_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+                flash('Bienvenido nuevamente')
+                redirect(url_for('hello'))
+            else:
+                flash('La información no coincide')
+
+        else:
+            flash('El usuario no existe')
+                
+
         return redirect(url_for('index'))
 
 
