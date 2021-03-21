6b8a2250d4e6d9906219a6de8e9fd43def4d0e25
raagus <raaguilarm@gmail.com>
2021-03-21 09:51:03 -0500

    Add new class TodoForm

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Enviar')
 
 
+class TodoForm(FlaskForm):
+    description = StringField('Description', validators=[DataRequired()])
+    submit = SubmitField('Crear')
+
