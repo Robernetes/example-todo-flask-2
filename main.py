5771f9be676bc6e21c214aa8c5efcd59da0063db
raagus <raaguilarm@gmail.com>
2021-03-21 09:54:07 -0500

    Add todo_form to context on hello endpoint and add if condition todo_form

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from flask_bootstrap import Bootstrap
 import unittest
 from flask_login import login_required, current_user
 from app import create_app
-from app.forms import LoginForm
+from app.forms import TodoForm
 from dotenv import load_dotenv
-from app.firestore_service import get_users, get_todos
+from app.firestore_service import get_users, get_todos, put_todo
 load_dotenv()
 
 app = create_app()
@@ -34,16 +34,23 @@ def index():
     session['user_ip'] = user_ip
     return response
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET','POST'])
 @login_required
 def hello():
     user_ip = session.get('user_ip')
     username = current_user.id
+    todo_form = TodoForm()
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username), 
-        'username': username
+        'username': username,
+        'todo_form': todo_form,
     }
+
+    if todo_form.validate_on_submit():
+        put_todo(user_id=username, description=todo_form.description.data)
+        flash('La tarea ha sido creada con exito')
+        return redirect(url_for('hello'))
 
     
 
